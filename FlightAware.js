67a0ebbc870f0f9fab8f71382bd1dfba8c25cfd8
JamesHensley <JamesHensley@users.noreply.github.com>
2020-12-21 14:55:58 +0100

    Update FlightAware.js

--- FlightAware.js
+++ FlightAware.js
@@ -1,3 +1,4 @@
+// Creates groups the data into InAir and OnGround collections
 var fetchUrl = 'https://flightaware.com/ajax/trackpoll.rvt?token=88dd7c1a0d41355dafa2ce4ff0e607704b11c422c132817794d3877132d3aaa34ae69c9170ff4785--364cc6200dbb7fcf5b4e272df5e944ceaef41605&locale=en_US&summary=1';
 
 fetch(fetchUrl)
@@ -14,3 +15,43 @@ fetch(fetchUrl)
 	};
 })
 .then(data => console.log(data))
+
+
+
+
+// Creates a GeoJSON of flights which have landed for importing into ArcGIS
+var fetchUrl = 'https://flightaware.com/ajax/trackpoll.rvt?token=88dd7c1a0d41355dafa2ce4ff0e607704b11c422c132817794d3877132d3aaa34ae69c9170ff4785--364cc6200dbb7fcf5b4e272df5e944ceaef41605&locale=en_US&summary=1';
+fetch(fetchUrl)
+.then(result => result.json())
+.then(data => data.flights)
+.then(data => Object.keys(data).map(d => {
+	data[d].flightId = d;
+	return data[d];
+}))
+.then(data => data.filter(f => f.landingTimes && f.landingTimes.actual != null))
+.then(data => {
+	return {
+		type: 'FeatureCollection',
+		features: data.map(d => {
+			return {
+				type: 'Feature',
+				geometry: {
+					type: 'LineString',
+					coordinates: d.track.map(m => m.coord)
+				},
+				properties: {
+					flightId: d.flightId
+				}
+			}
+		})
+	}
+})
+.then(geoJson => {
+	let dElem = document.createElement('div');
+	let cElem = document.createElement('code');
+	document.body.appendChild(dElem);
+	document.body.appendChild(cElem);
+	cElem.innerText = JSON.stringify(geoJson);
+
+	console.log(geoJson)
+});
