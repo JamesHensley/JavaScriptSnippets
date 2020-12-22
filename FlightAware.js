f927c48c6e3d0fe5b6a75290c3817e020f908810
JamesHensley <JamesHensley@users.noreply.github.com>
2020-12-22 16:04:02 +0100

    Update FlightAware.js

--- FlightAware.js
+++ FlightAware.js
@@ -20,7 +20,7 @@ fetch(fetchUrl)
 
 
 // Creates a GeoJSON of flights which have landed for importing into ArcGIS
-var fetchUrl = 'https://flightaware.com/ajax/trackpoll.rvt?token=88dd7c1a0d41355dafa2ce4ff0e607704b11c422c13281777c192da6458ee4dac7bac9c3e7d3966a--191f3494adc1b5f35896037658161abc1755299a&locale=en_US&summary=0';
+var fetchUrl = 'https://flightaware.com/ajax/trackpoll.rvt?token=88dd7c1a0d41355dafa2ce4ff0e607704b11c422c13281775651937b72b4fb06cf96a80adf3baec3--19461d238fdf1821397aa69593ea0dbc0dee55c2&locale=en_US&summary=0';
 fetch(fetchUrl)
 .then(result => result.json())
 .then(data => data.flights)
@@ -33,11 +33,25 @@ fetch(fetchUrl)
 	return {
 		type: 'FeatureCollection',
 		features: data.map(d => {
+			// We have to do this crazy coordinate thing to handle cross-meridian lines
+			//   or a straight line will be drawn across the screen when crossing the
+			//   antimeridian... multiline string is perfect for this.
+			// newCoords will be an array of arrays for each segment which crosses a
+			//   meridian and can handle multiple paths zig-zagging back & forth
+			var newCoords = d.track.reduce((t, n, i, e) => {
+				// Checks to see if the current longitude's sign differs from the previous longitude's sign
+				// TODO: Ignore prime-meridian cosses since most software handles this fairly well
+				let crossedMeridian = i > 0 ? !((e[i].coord[0] < 150) == (e[i-1].coord[0] < 150)) : false;
+				if(i == 0 || crossedMeridian) { t.push([]); }
+				t[t.length-1].push(n.coord);
+				return t;
+			}, []);
+
 			return {
 				type: 'Feature',
 				geometry: {
-					type: 'LineString',
-					coordinates: d.track.map(m => m.coord)
+					type: 'MultiLineString',
+					coordinates: newCoords
 				},
 				properties: {
 					flightId: d.flightId,
@@ -50,15 +64,16 @@ fetch(fetchUrl)
 	}
 })
 .then(geoJson => {
-	document.querySelectorAll('code[id="esriThingy"]').forEach(d => document.body.removeChild(d));
+	document.querySelectorAll('div[id="esriThingy"]').forEach(d => document.body.removeChild(d));
 	let dElem = document.createElement('div');
-	let bElem = document.createElement('button');
+		dElem.setAttribute('id', 'esriThingy');
 	let cElem = document.createElement('code');
-	cElem.setAttribute('id', 'esriThingy');
-	document.body.appendChild(dElem);
+		cElem.innerText = JSON.stringify(geoJson);
+	let bElem = document.createElement('button');
+		bElem.innerText = 'Copy To Clipboard';
+		bElem.setAttribute('onClick', "javascript:navigator.clipboard.writeText(document.querySelector('div[id=\"esriThingy\"] code').innerText)");
 	dElem.appendChild(bElem);
 	dElem.appendChild(cElem);
-	cElem.innerText = JSON.stringify(geoJson);
-	bElem.onClick = () => { navigator.clipboard.writeText(JSON.stringify(geoJson)); };
+	document.body.appendChild(dElem);
 	console.log(geoJson);
-})
+});
