703c72191ac1811baabc08e02248878fcbe6c3d1
JamesHensley <JamesHensley@users.noreply.github.com>
2020-12-22 10:22:38 +0100

    Update FlightAware.js

--- FlightAware.js
+++ FlightAware.js
@@ -20,7 +20,7 @@ fetch(fetchUrl)
 
 
 // Creates a GeoJSON of flights which have landed for importing into ArcGIS
-var fetchUrl = 'https://flightaware.com/ajax/trackpoll.rvt?token=88dd7c1a0d41355dafa2ce4ff0e607704b11c422c132817794d3877132d3aaa34ae69c9170ff4785--364cc6200dbb7fcf5b4e272df5e944ceaef41605&locale=en_US&summary=1';
+var fetchUrl = 'https://flightaware.com/ajax/trackpoll.rvt?token=88dd7c1a0d41355dafa2ce4ff0e607704b11c422c13281777c192da6458ee4dac7bac9c3e7d3966a--191f3494adc1b5f35896037658161abc1755299a&locale=en_US&summary=0';
 fetch(fetchUrl)
 .then(result => result.json())
 .then(data => data.flights)
@@ -41,17 +41,24 @@ fetch(fetchUrl)
 				},
 				properties: {
 					flightId: d.flightId,
-					carrier: d.flightId.match(/^([a-zA-Z]*)/)[1] || 'Unknown'
+					carrier: d.flightId.match(/^([a-zA-Z]*)/)[1],
+					takeoff: new Date((d.takeoffTimes.actual || 0) * 1000),
+					landed: new Date((d.landingTimes.actual || 0) * 1000)
 				}
 			}
 		})
 	}
 })
 .then(geoJson => {
+	document.querySelectorAll('code[id="esriThingy"]').forEach(d => document.body.removeChild(d));
 	let dElem = document.createElement('div');
+	let bElem = document.createElement('button');
 	let cElem = document.createElement('code');
+	cElem.setAttribute('id', 'esriThingy');
 	document.body.appendChild(dElem);
-	document.body.appendChild(cElem);
+	dElem.appendChild(bElem);
+	dElem.appendChild(cElem);
 	cElem.innerText = JSON.stringify(geoJson);
-	console.log(geoJson)
+	bElem.onClick = () => { navigator.clipboard.writeText(JSON.stringify(geoJson)); };
+	console.log(geoJson);
 })
