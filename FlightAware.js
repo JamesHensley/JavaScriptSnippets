14d3ef554142948909f475aefa40c1c23033db2f
JamesHensley <JamesHensley@users.noreply.github.com>
2020-12-23 12:27:34 +0100

    Update FlightAware.js

--- FlightAware.js
+++ FlightAware.js
@@ -1,26 +1,6 @@
 // Creates groups the data into InAir and OnGround collections
-var fetchUrl = 'https://flightaware.com/ajax/trackpoll.rvt?token=88dd7c1a0d41355dafa2ce4ff0e607704b11c422c132817794d3877132d3aaa34ae69c9170ff4785--364cc6200dbb7fcf5b4e272df5e944ceaef41605&locale=en_US&summary=1';
+var fetchUrl = 'https://flightaware.com/ajax/trackpoll.rvt?token=88dd7c1a0d41355d976ff0058b24601b6dd7489867c9cb0a--ea2472772572b854fc56c7b1dcd7c03d6da8702d&locale=en_US&summary=0';
 
-fetch(fetchUrl)
-.then(result => result.json())
-.then(data => data.flights)
-.then(data => Object.keys(data).map(d => {
-	data[d].flightId = d;
-	return data[d];
-}))
-.then(data => {
-	return {
-		inAir: data.filter(f => f.landingTimes && f.landingTimes.actual == null),
-		onGround: data.filter(f => f.landingTimes && f.landingTimes.actual != null)
-	};
-})
-.then(data => console.log(data))
-
-
-
-
-// Creates a GeoJSON of flights which have landed for importing into ArcGIS
-var fetchUrl = 'https://flightaware.com/ajax/trackpoll.rvt?token=88dd7c1a0d41355dafa2ce4ff0e607704b11c422c13281775651937b72b4fb06cf96a80adf3baec3--19461d238fdf1821397aa69593ea0dbc0dee55c2&locale=en_US&summary=0';
 fetch(fetchUrl)
 .then(result => result.json())
 .then(data => data.flights)
@@ -40,8 +20,7 @@ fetch(fetchUrl)
 			//   meridian and can handle multiple paths zig-zagging back & forth
 			var newCoords = d.track.reduce((t, n, i, e) => {
 				// Checks to see if the current longitude's sign differs from the previous longitude's sign
-				// TODO: Implement better way of handling AntiMeridian crossings rather than using hard-coded
-				//   numbers (ie: 150)
+				// TODO: Ignore prime-meridian cosses since most software handles this fairly well
 				let crossedMeridian = i > 0 ? !((e[i].coord[0] < 150) == (e[i-1].coord[0] < 150)) : false;
 				if(i == 0 || crossedMeridian) { t.push([]); }
 				t[t.length-1].push(n.coord);
@@ -75,6 +54,6 @@ fetch(fetchUrl)
 		bElem.setAttribute('onClick', "javascript:navigator.clipboard.writeText(document.querySelector('div[id=\"esriThingy\"] code').innerText)");
 	dElem.appendChild(bElem);
 	dElem.appendChild(cElem);
-	document.body.appendChild(dElem);
+	document.body.insertBefore(dElem, document.body.firstChild)
 	console.log(geoJson);
 });
